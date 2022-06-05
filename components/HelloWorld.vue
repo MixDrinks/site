56e2fa093085963de729a951970970911652668b
BuJlJlu <golovashchenkom@ukr.net>
2022-06-05 13:12:10 +0300

    init

---
+++ components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello-world">hello world</div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+};
+</script>
+
+<style lang="scss" scoped>
+.hello-world {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 50px;
+}
+</style>
