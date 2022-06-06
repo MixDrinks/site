66a6574ea742e4d5a3fa1fafe3b79516ff716d4f
BuJlJlu <golovashchenkom@ukr.net>
2022-06-06 23:56:53 +0300

    update request configuration

--- api/index.js
+++ api/index.js
@@ -5,9 +5,11 @@ $axios.defaults.withCredentials = true;
 export const api = $axios.create({
   baseURL: process.env.api,
   headers: {
+    "Access-Control-Allow-Credentials": true,
     "Content-Type": "application/hal+json",
     Accept: "application/json",
   },
+  credentials: true,
 });
 
 export const getСocktails = () => {
