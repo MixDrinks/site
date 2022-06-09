98ac92e9cd38974c077484bba793c49ff6a76b37
BuJlJlu <golovashchenkom@ukr.net>
2022-06-09 20:31:54 +0300

    test fix

--- pages/cocktails/_id.vue
+++ pages/cocktails/_id.vue
@@ -1,11 +1,122 @@
 <template>
   <main class="wrapper">
-    <CocktailPage :cocktail="cocktail" />
+    <div class="cocktail">
+      <div class="cocktail__header">
+        <h1 class="cocktail__title">{{ cocktail.name }}</h1>
+        <ul class="cocktail__tags tags">
+          <li class="tags__item" v-for="tag in cocktail.tags" :key="tag.id">
+            <NuxtLink :to="`/cocktails?tags=${tag.id}`" class="tags__link">
+              <span class="tags__name">{{ tag.name }}</span>
+            </NuxtLink>
+          </li>
+        </ul>
+      </div>
+      <div class="cocktail__body">
+        <div class="cocktail__img">
+          <picture>
+            <source
+              v-for="img in cocktail.images"
+              :key="img.id"
+              :srcset="img.srcset"
+              :media="img.media"
+              :type="img.type"
+            />
+            <img
+              class="img"
+              width="500"
+              height="500"
+              loading="lazy"
+              alt=""
+              title=""
+            />
+          </picture>
+        </div>
+        <div class="cocktail__recipe recipe">
+          <div class="recipe__title">Рецепт коктейлю {{ cocktail.name }}</div>
+          <ol class="recipe__list">
+            <li
+              class="recipe__item"
+              v-for="recipeItem in cocktail.receipt"
+              :key="recipeItem"
+            >
+              <div class="recipe__text">{{ recipeItem }}</div>
+            </li>
+          </ol>
+        </div>
+        <ul class="cocktail__goods goods">
+          <div class="goods__title">Склад коктейлю {{ cocktail.name }}</div>
+          <ul class="goods__list">
+            <li
+              class="goods__item"
+              v-for="good in cocktail.goods"
+              :key="good.name"
+            >
+              <div class="goods__img">
+                <picture>
+                  <source
+                    v-for="img in good.images"
+                    :key="img.id"
+                    :srcset="img.srcset"
+                    :media="img.media"
+                    :type="img.type"
+                  />
+                  <img
+                    class="img"
+                    width="150"
+                    height="150"
+                    loading="lazy"
+                    alt=""
+                    title=""
+                  />
+                </picture>
+              </div>
+              <div class="goods__name">{{ good.name }}</div>
+            </li>
+          </ul>
+        </ul>
+
+        <div class="cocktail__tools tools">
+          <div class="tools__title">
+            Потрібні штучки для пригоування {{ cocktail.name }}
+          </div>
+          <ul class="tools__list">
+            <li
+              class="tools__item"
+              v-for="tool in cocktail.tools"
+              :key="tool.id"
+            >
+              <div class="tools__img">
+                <picture>
+                  <source
+                    v-for="img in tool.images"
+                    :key="img.id"
+                    :srcset="img.srcset"
+                    :media="img.media"
+                    :type="img.type"
+                  />
+                  <img
+                    class="img"
+                    width="150"
+                    height="150"
+                    loading="lazy"
+                    alt=""
+                    title=""
+                  />
+                </picture>
+              </div>
+              <div class="tools__name">{{ tool.name }}</div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="cocktail__footer"></div>
+    </div>
+    <!-- <CocktailPage :cocktail="cocktail" /> -->
   </main>
 </template>
 
 <script>
-import CocktailPage from "~~/components/cocktails/CocktailPage.vue";
+// import CocktailPage from "~~/components/cocktails/CocktailPage.vue";
 import { getCocktail } from "~~/api";
 export default {
   async asyncData({ route, error }) {
@@ -27,5 +138,182 @@ export default {
 <style lang="scss" scoped>
 .wrapper {
   @include defaultWrapper;
+}
+.cocktail {
+  &__title {
+    @include fontSize48B;
+  }
+  &__tags {
+    margin-top: $shortMargin;
+    margin-bottom: $shortMargin;
+  }
+  &__img {
+    flex-basis: 500px;
+
+    margin-bottom: $shortMargin;
+
+    .img {
+      display: block;
+      object-fit: cover;
+    }
+  }
+  &__body {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  &__recipe {
+    margin-bottom: $shortMargin;
+
+    flex-basis: calc(100% - 500px);
+    padding-left: 100px;
+  }
+  &__goods {
+    flex-basis: 100%;
+    margin-bottom: $shortMargin;
+  }
+  &__tools {
+    flex-basis: 100%;
+    margin-bottom: $shortMargin;
+  }
+}
+.tools {
+  &__title {
+    color: $colorBlack;
+    @include fontSize24B;
+
+    margin-bottom: $shortMargin;
+  }
+  &__list {
+    display: flex;
+    flex-wrap: wrap;
+    margin: -10px;
+  }
+  &__item {
+    padding: 10px;
+  }
+  &__name {
+    max-width: 150px;
+    color: $colorMain;
+    @include fontSize14;
+    text-align: center;
+
+    margin-top: 12px;
+  }
+  &__img {
+    border-radius: 8px;
+    overflow: hidden;
+    border: 1px solid $colorMain;
+    .img {
+      display: block;
+      object-fit: cover;
+    }
+  }
+}
+.goods {
+  &__title {
+    color: $colorBlack;
+    @include fontSize24B;
+
+    margin-bottom: $shortMargin;
+  }
+  &__list {
+    display: flex;
+    flex-wrap: wrap;
+    margin: -10px;
+  }
+  &__item {
+    padding: 10px;
+  }
+  &__name {
+    max-width: 150px;
+    color: $colorMain;
+    @include fontSize14;
+    text-align: center;
+
+    margin-top: 12px;
+  }
+  &__img {
+    border-radius: 8px;
+    overflow: hidden;
+    border: 1px solid $colorMain;
+    .img {
+      display: block;
+      object-fit: cover;
+    }
+  }
+}
+
+.recipe {
+  &__title {
+    color: $colorBlack;
+    @include fontSize24B;
+
+    margin-bottom: $shortMargin;
+  }
+  list-style: none;
+  counter-reset: counter;
+  &__item {
+    position: relative;
+
+    display: flex;
+    align-items: center;
+
+    counter-increment: counter;
+    &::before {
+      @include fontSize18B;
+      color: $colorWhite;
+
+      width: 40px;
+      height: 40px;
+
+      margin-right: $halfShortMargin;
+
+      border-radius: 4px;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-basis: 40px;
+
+      background-color: $colorMain;
+
+      content: counter(counter);
+    }
+    &:not(:last-child) {
+      margin-bottom: $halfShortMargin;
+      padding-bottom: $halfShortMargin;
+
+      border-bottom: 1px solid $colorMain;
+    }
+  }
+  &__text {
+    flex-basis: calc(100% - 64px);
+    @include fontSize18M;
+  }
+}
+.tags {
+  display: flex;
+
+  &__item {
+    &:not(:last-child) {
+      margin-right: 10px;
+    }
+  }
+  &__name {
+    @include fontSize18B;
+    display: block;
+    padding: 5px 10px;
+    border-radius: 8px;
+    background-color: rgba($colorBlack, 0.4);
+    color: $colorWhite;
+
+    transition: background-color $defaultAnimTime;
+    &::first-letter {
+      text-transform: uppercase;
+    }
+    &:hover {
+      background-color: $colorHover;
+    }
+  }
 }
 </style>
