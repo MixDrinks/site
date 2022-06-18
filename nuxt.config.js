e5c207c7e21e17d318d7a6ba2e542c54488a5fd3
BuJlJlu <golovashchenkom@ukr.net>
2022-06-18 13:50:09 +0300

    add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,13 +25,39 @@ module.exports = {
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { name: "robots", content: "noindex, nofollow" },
+      { name: "msapplication-TileColor", content: "#603cba" },
+      { name: "theme-color", content: "#ffffff" },
       {
         hid: "description",
         name: "description",
         content: process.env.npm_package_description || "",
       },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/favicon/apple-touch-icon.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon/favicon-16x16.png",
+      },
+      { rel: "manifest", href: "/favicon/site.webmanifest" },
+      {
+        rel: "mask-icon",
+        href: "/favicon/safari-pinned-tab.svg",
+        color: "#5bbad5",
+      },
+    ],
   },
   router: {
     prefetchLinks: false,
