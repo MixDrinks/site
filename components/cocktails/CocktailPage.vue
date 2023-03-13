38c971733b4046723bef1362f47dc06dd22e1b95
Developer <promotion.x.developer1@gmail.com>
2023-03-13 09:01:19 +0200

    update cocktail components

--- components/cocktails/CocktailPage.vue
+++ components/cocktails/CocktailPage.vue
@@ -58,91 +58,8 @@
           </li>
         </ol>
       </div>
-      <div class="cocktail-body__goods cocktail-body-goods">
-        <h2 class="cocktail-body-goods__title cocktail-body-goods-title">
-          <span class="cocktail-body-goods-title__label">
-            Склад коктейлю {{ cocktail.name }}
-          </span>
-        </h2>
-        <div class="cocktail-body-goods__counter cocktail-body-goods-counter">
-          <IconBtn
-              class="cocktail-body-goods-counter__btn cocktail-body-goods-counter__btn--dec"
-              :lock="counter === 1"
-              icon="/img/icons/minus.svg"
-              @click="dec()"/>
-
-          <div class="cocktail-body-goods-counter__value">
-            {{ counter }}
-          </div>
-          <IconBtn
-              class="cocktail-body-goods-counter__btn cocktail-body-goods-counter__btn--inc"
-              icon="/img/icons/plus.svg"
-              @click="inc()"
-          />
-        </div>
-        <ul class="cocktail-body-goods__list cocktail-body-goods-list">
-          <li class="cocktail-body-goods-list__item cocktail-body-goods-list-item" v-for="good in cocktail.goods" :key="good.id">
-            <NuxtLink class="cocktail-body-goods-list-item__link cocktail-body-goods-list-item-link" :to="`/goods/${good.id}`">
-              <picture
-                class="cocktail-body-goods-list-item-link__picture">
-                  <source
-                    v-for="img in good.images"
-                    :key="img.id"
-                    :srcset="img.srcset"
-                    :media="img.media"
-                    :type="img.type"
-                  />
-                  <img
-                    class="cocktail-body-goods-list-item-link__img"
-                    width="150"
-                    height="150"
-                    loading="lazy"
-                    :alt="`Зображення ${good.name}`"
-                    title=""
-                  />
-                </picture>
-              <div class="cocktail-body-goods-list-item-link__label">
-                {{ good.name }}<br /><strong>
-                  {{ good.amount * counter }} {{ good.unit }}.
-                </strong>
-              </div>
-            </NuxtLink>
-          </li>
-        </ul>
-      </div>
-      <div class="cocktail-body__tools cocktail-body-tools">
-        <h2 class="cocktail-body-tools__title cocktail-body-tools-title">
-          <span class="cocktail-body-tools-title__label">
-            Потрібні штучки для пригоування {{ cocktail.name }}
-          </span>
-        </h2>
-        <ul class="cocktail-body-tools__list cocktail-body-tools-list">
-          <li class="cocktail-body-tools-list__item cocktail-body-tools-list-item" v-for="tool in cocktail.tools" :key="tool.id">
-            <NuxtLink class="cocktail-body-tools-list-item__link cocktail-body-tools-list-item-link" :to="`/tools/${tool.id}`">
-              <picture class="cocktail-body-tools-list-item-link__picture">
-                  <source
-                    v-for="img in tool.images"
-                    :key="img.id"
-                    :srcset="img.srcset"
-                    :media="img.media"
-                    :type="img.type"
-                  />
-                  <img
-                    class="cocktail-body-tools-list-item-link__img"
-                    width="150"
-                    height="150"
-                    loading="lazy"
-                    :alt="`Зображення ${tool.name}`"
-                    title=""
-                  />
-                </picture>
-              <div class="cocktail-body-tools-list-item-link__label">
-                {{ tool.name }}
-              </div>
-            </NuxtLink>
-          </li>
-        </ul>
-      </div>
+      <CocktailComponents class="cocktail-body__goods" :title="`Склад коктейлю ${cocktail.name}`" :components="cocktail.goods" withCounter />
+      <CocktailComponents class="cocktail-body__tools" :title="`Потрібні штучки для пригоування ${cocktail.name}`" :components="cocktail.tools" />
     </div>
   </div>
 </template>
@@ -150,24 +67,11 @@
 <script>
 import Rating from "~~/components/cocktails/Rating.vue";
 import IconBtn from "~/components/dump/UI/buttons/IconBtn.vue";
+import CocktailComponents from "~~/components/cocktails/CocktailComponents.vue";
 
 export default {
   name: "CocktailPage",
-  components: {IconBtn, Rating },
-  data: () => ({
-        counter: 1
-      }
-  ),
-  methods: {
-    inc() {
-      this.counter++
-    },
-    dec() {
-      if (this.counter > 1) {
-        this.counter--
-      }
-    }
-  },
+  components: { IconBtn, Rating, CocktailComponents },
   props: {
     cocktail: {
       type: Object,
