8bff6a76cd0fe42fa89533e9a7cfc725dec13fa3
tanyaee <eremeeva.tallium@gmail.com>
2023-05-15 17:02:24 +0100

    cleanup

--- cypress/tests/home-mobile.cy.js
+++ cypress/tests/home-mobile.cy.js
@@ -1,28 +1,6 @@
 import { default as homePage } from '../support/page_objects/homePage';
 import { default as cocktailPage } from '../support/page_objects/cocktailPage';
-
-const defaultSizes = ['iphone-6', 'ipad-2', 'macbook-13'];
-
-function forEverySize(options) {
-    let tests;
-    let sizes;
-    if (typeof options === 'function') {
-        tests = options;
-        sizes = defaultSizes;
-    } else {
-        ({ sizes = defaultSizes, tests } = options);
-    }
-    sizes.forEach((size) => {
-        context(`for ${size} screen size`, () => {
-            beforeEach(() => {
-                cy.viewport(size);
-            });
-            tests({ viewport: size });
-        });
-    });
-}
-
-export default forEverySize;
+import forEverySize from '../helpers/for-every-size'
 
 describe("Home screen elements on the different screen resolutions", () => {
 forEverySize(({ viewport }) => {
