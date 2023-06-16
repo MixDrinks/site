f2ad301d36b666fa5ef36d96bff1294d9ea223a1
tanyaee <eremeeva.tallium@gmail.com>
2023-06-16 10:55:26 +0100

    fixed failing tests

--- cypress/tests/home-mobile.cy.js
+++ cypress/tests/home-mobile.cy.js
@@ -9,49 +9,49 @@ describe("Home screen elements on the different screen resolutions", () => {
       cy.visit("/");
     });
 
-    // it("can view the cocktails list and open the detailed cocktail screen", () => {
-    //   cy.intercept({
-    //       method: "GET",
-    //       url: "/v2/cocktails/full*",
-    //     }).as("cocktail");
-    //   cy.request({
-    //     method: 'GET',
-    //     url: 'https://whale-app-iz3av.ondigitalocean.app/v2/search/cocktails?page=0',
-    //     headers: {
-    //       'Content-Type': 'application/json'
-    //     }
-    //   }).then((cocktailsList) => {
-    //     const cocktails = cocktailsList.body.cocktails
-    //     cy.get(homePage.vertical.selector.sorting.byRate).should("be.visible")
-    //     cy.get(homePage.vertical.selector.search).should("be.visible")
-    //     cy.get(homePage.vertical.selector.cocktailCard).each(item => {
-    //       const itemText = item.text().trim()
-    //       expect(itemText).to.contain(cocktails[item.index()].name)
-    //     })
-    //     cy.get(homePage.vertical.selector.cocktailCard).eq(5).click()
+    it("can view the cocktails list and open the detailed cocktail screen", () => {
+      cy.intercept({
+          method: "GET",
+          url: "/v2/cocktails/full*",
+        }).as("cocktail");
+      cy.request({
+        method: 'GET',
+        url: 'https://whale-app-iz3av.ondigitalocean.app/v2/filter?page=0',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then((cocktailsList) => {
+        const cocktails = cocktailsList.body.cocktails
+        cy.get(homePage.vertical.selector.sorting.byRate).should("be.visible")
+        cy.get(homePage.vertical.selector.search).should("be.visible")
+        cy.get(homePage.vertical.selector.cocktailCard).each(item => {
+          const itemText = item.text().trim()
+          expect(itemText).to.contain(cocktails[item.index()].name)
+        })
+        cy.get(homePage.vertical.selector.cocktailCard).eq(5).click()
 
-    //     cy.wait('@cocktail').then( cocktail => {
-    //       const details = cocktail.response.body;
-    //       const receipt = details.receipt
-    //       const goods = details.goods
-    //       const tools = details.tools
+        cy.wait('@cocktail').then( cocktail => {
+          const details = cocktail.response.body;
+          const receipt = details.receipt
+          const goods = details.goods
+          const tools = details.tools
 
-    //       cy.get(cocktailPage.vertical.selector.title).should('contain', details.name)
-    //       cy.get(cocktailPage.vertical.selector.recipeItem).each(listItem => {
-    //         const itemText = listItem.text()
-    //         expect(itemText).to.equal(receipt[listItem.index()])
-    //       })
-    //       cy.get(cocktailPage.vertical.selector.component).find('[data-cy="ingredient"]').each(listItem => {
-    //         const itemText = listItem.text()
-    //         expect(itemText).contains(goods[listItem.index()].name)
-    //       })
-    //       cy.get(cocktailPage.vertical.selector.tools).find('[data-cy="ingredient"]').each(listItem => {
-    //         const itemText = listItem.text()
-    //         expect(itemText).contains(tools[listItem.index()].name)
-    //       })
-    //     })
+          cy.get(cocktailPage.vertical.selector.title).should('contain', details.name)
+          cy.get(cocktailPage.vertical.selector.recipeItem).each(listItem => {
+            const itemText = listItem.text()
+            expect(itemText).to.equal(receipt[listItem.index()])
+          })
+          cy.get(cocktailPage.vertical.selector.component).find('[data-cy="ingredient"]').each(listItem => {
+            const itemText = listItem.text()
+            expect(itemText).contains(goods[listItem.index()].name)
+          })
+          cy.get(cocktailPage.vertical.selector.tools).find('[data-cy="ingredient"]').each(listItem => {
+            const itemText = listItem.text()
+            expect(itemText).contains(tools[listItem.index()].name)
+          })
+        })
 
-    //   })
-    // });
+      })
+    });
   });
 });
--- cypress/tests/home-screen.cy.js
+++ cypress/tests/home-screen.cy.js
@@ -4,14 +4,20 @@ describe("Home screen tests", () => {
   const interceptSorting = (sortingParam) => {
     cy.intercept({
       method: "GET",
-      url: `/v2/search/cocktails?page=0&${sortingParam}`,
+      url: `/v2/filter?${sortingParam}&page=0`,
+    }).as("sortingApplied");
+  };
+  const interceptFiltering = (filterParam) => {
+    cy.intercept({
+      method: "GET",
+      url: `/v2/filter/${filterParam}?page=0`,
     }).as("sortingApplied");
   };
   beforeEach("Open home screen", () => {
     cy.visit("/");
   });
 
-  it.skip("items should be reordered after sorting applying", () => {
+  it("items should be reordered after sorting applying", () => {
     interceptSorting("sort=biggest-rate");
 
     cy.get(homePage.vertical.selector.sorting.byRate).click();
@@ -28,29 +34,29 @@ describe("Home screen tests", () => {
       });
   });
 
-  // it("items should be filteres after applying a low alcohol filter", () => {
-  //   interceptSorting("alcohol-volume=1")
+  it("items should be filtered after applying a low alcohol filter", () => {
+    interceptFiltering("alcohol-volume=slaboalkoholni")
 
-  //   let firstItemName
-  //   cy.get(homePage.vertical.selector.cocktailCard).find(".cart__name").first().invoke('text').then((itemTitle) => {
-  //     firstItemName = itemTitle
-  //   })
+    let firstItemName
+    cy.get(homePage.vertical.selector.cocktailCard).find(".cart__name").first().invoke('text').then((itemTitle) => {
+      firstItemName = itemTitle
+    })
 
-  //   // applying the low alcohol filter
-  //   cy.get(homePage.vertical.selector.filters.lowalcohol).click()
-  //   // checking that proper tag is shown
-  //   cy.get(homePage.vertical.selector.filters.lowalcoholTag).should('be.visible')
-  //   // applying the low alcohol filter again because when it clicked programatically the request is not sent
-  //   cy.get(homePage.vertical.selector.filters.lowalcohol).click()
+    // applying the low alcohol filter
+    cy.get(homePage.vertical.selector.filters.lowalcohol).click()
+    // checking that proper tag is shown
+    cy.get(homePage.vertical.selector.filters.lowalcoholTag).should('be.visible')
+    // applying the low alcohol filter again because when it clicked programatically the request is not sent
+    cy.get(homePage.vertical.selector.filters.lowalcohol).click()
 
-  //   cy.wait("@sortingApplied").its('response.body').then( (body) => {
-  //     const cocktails = body.cocktails
-  //     cy.get(homePage.vertical.selector.cocktailCard).each(item => {
-  //       const itemText = item.text().trim()
-  //       expect(itemText).to.contain(cocktails[item.index()].name)
-  //     })
-  //     // checking that the first element name is changed after applying filters
-  //     cy.get(homePage.vertical.selector.cocktailCard).find(".cart__name").first().should('not.contain', firstItemName)
-  //   });
-  // });
+    cy.wait("@sortingApplied").its('response.body').then( (body) => {
+      const cocktails = body.cocktails
+      cy.get(homePage.vertical.selector.cocktailCard).each(item => {
+        const itemText = item.text().trim()
+        expect(itemText).to.contain(cocktails[item.index()].name)
+      })
+      // checking that the first element name is changed after applying filters
+      cy.get(homePage.vertical.selector.cocktailCard).find(".cart__name").first().should('not.contain', firstItemName)
+    });
+  });
 });
