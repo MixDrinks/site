ec65ac0a4c9aed159f048e8ccab51fa7bf396937
tanyaee <eremeeva.tallium@gmail.com>
2023-06-16 11:28:22 +0100

    separate files added for sorting and filtering tests

--- cypress/tests/home-screen-filtering.cy.js
+++ cypress/tests/home-screen-filtering.cy.js
@@ -1,12 +1,6 @@
 import { default as homePage } from "../support/page_objects/homePage";
 
 describe("Home screen tests", () => {
-  const interceptSorting = (sortingParam) => {
-    cy.intercept({
-      method: "GET",
-      url: `/v2/filter?${sortingParam}*`,
-    }).as("sortingApplied");
-  };
   const interceptFiltering = (filterParam) => {
     cy.intercept({
       method: "GET",
@@ -15,23 +9,6 @@ describe("Home screen tests", () => {
   };
   beforeEach("Open home screen", () => {
     cy.visit("/");
-  });
-
-  it("items should be reordered after sorting applying", () => {
-    interceptSorting("sort=biggest-rate");
-
-    cy.get(homePage.vertical.selector.sorting.byRate).click();
-    cy.get(homePage.vertical.selector.sorting.byRate).click();
-    cy.get(homePage.vertical.selector.sorting.byRate).click();
-    cy.wait("@sortingApplied")
-      .its("response.body")
-      .then((body) => {
-        const cocktails = body.cocktails;
-        cy.get(homePage.vertical.selector.cocktailCard).each((item) => {
-          const itemText = item.text().trim();
-          expect(itemText).to.contain(cocktails[item.index()].name);
-        });
-      });
   });
 
   it("items should be filtered after applying a low alcohol filter", () => {
---
+++ cypress/tests/home-screen-sorting.cy.js
@@ -0,0 +1,30 @@
+import { default as homePage } from "../support/page_objects/homePage";
+
+describe("Home screen tests", () => {
+  const interceptSorting = (sortingParam) => {
+    cy.intercept({
+      method: "GET",
+      url: `/v2/filter?${sortingParam}*`,
+    }).as("sortingApplied");
+  };
+
+  beforeEach("Open home screen", () => {
+    cy.visit("/");
+  });
+  it("items should be reordered after sorting applying", () => {
+    interceptSorting("sort=biggest-rate");
+
+    cy.get(homePage.vertical.selector.sorting.byRate).click();
+    cy.get(homePage.vertical.selector.sorting.byRate).click();
+    cy.get(homePage.vertical.selector.sorting.byRate).click();
+    cy.wait("@sortingApplied")
+      .its("response.body")
+      .then((body) => {
+        const cocktails = body.cocktails;
+        cy.get(homePage.vertical.selector.cocktailCard).each((item) => {
+          const itemText = item.text().trim();
+          expect(itemText).to.contain(cocktails[item.index()].name);
+        });
+      });
+  });
+});
