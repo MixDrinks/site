e94552cac2e9ceaee8a7a3d8fb8add2ce34ddc40
vovastelmashchuk <vovochkastelmashchuk@gmail.com>
2024-10-05 13:52:42 +0300

    add end point for get posts by tags

---
+++ server/api/blog/tag/[tag].js
@@ -0,0 +1,34 @@
+import { defineEventHandler } from 'h3'
+import { db } from '~/server/utils/mongo'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+const imageDomain = process.env.IMAGE_DOMAIN
+
+async function getPostsWithTag(tagSlug) {
+    return db
+        .collection('blog')
+        .find({ 'tags.slug': tagSlug })
+        .project({ _id: 0, slug: 1, title: 1, image: 1 })
+        .toArray()
+}
+
+export default defineEventHandler(async (req) => {
+    const tagSlug = req.context.params.tag
+
+    const postList = await getPostsWithTag(tagSlug)
+
+    const responsePostList = postList.map((post) => {
+        const imageFullUrl = `${imageDomain}/${post.image}`
+
+        const postDto = {
+            slug: post.slug,
+            title: post.title,
+            image: imageFullUrl
+        }
+        return postDto
+    })
+
+    return responsePostList
+})
