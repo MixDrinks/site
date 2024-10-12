b65dd6098df02f9d56b1d7d682663097eb91353d
BuJlJlu <golovashchenkom@ukr.net>
2024-10-12 13:07:14 +0300

    update advertising

--- components/global/Advertising.vue
+++ components/global/Advertising.vue
@@ -10,29 +10,44 @@
 </template>
 
 <script>
-
-import { defineComponent, onMounted } from 'vue'
+import { defineComponent, onBeforeUnmount, onMounted } from 'vue'
 
 export default defineComponent({
     name: 'Advertising',
 
     setup() {
-        function loadAdsbygoogle() {
-            const script = document.createElement('script');
-            script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9033785625371866";
-            script.async = true;
-            document.body.appendChild(script);
+        function loadAdsByGoogle() {
+            const script = document.createElement('script')
+            script.src =
+                'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9033785625371866'
+            script.async = true
+            document.body.appendChild(script)
 
-            script.onload = function() {
+            script.onload = function () {
                 ;(adsbygoogle = window.adsbygoogle || []).push({})
-            };
+            }
         }
+
+        const loadFun = () => {
+            if (!window.adsbygoogle) {
+                loadAdsByGoogle()
+            }
+        }
+
+        onMounted(() => {
+            if (document.readyState === 'complete') {
+                loadFun()
+            } else {
+                window.addEventListener('load', loadFun)
+            }
+        })
+        onBeforeUnmount(() => {
+            window.removeEventListener('load', loadFun)
+        })
 
         onMounted(() => {
             if (window.adsbygoogle) {
                 ;(adsbygoogle = window.adsbygoogle || []).push({})
-            } else {
-                loadAdsbygoogle();
             }
         })
     }
