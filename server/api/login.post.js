0ba7e442f24d867202e051438db67b016fb6a06b
vovastelmashchuk <vovochkastelmashchuk@gmail.com>
2024-10-18 23:05:51 +0300

    add login

---
+++ server/api/login.post.js
@@ -0,0 +1,31 @@
+import { defineEventHandler, readBody, setCookie, setResponseStatus } from 'h3'
+import { addSessionToUser } from '~/server/utils/user'
+
+export default defineEventHandler(async (event) => {
+    const body = await readBody(event)
+    const { username, password } = body
+
+    const { sessionId, expiresAt, error } = await addSessionToUser(
+        username,
+        password
+    )
+
+    if (error) {
+        setResponseStatus(event, 401)
+        return {
+            error: 'Invalid username or password'
+        }
+    }
+
+    if (sessionId && expiresAt) {
+        setCookie(event, 'sessionId', sessionId)
+        return {
+            status: 'success'
+        }
+    }
+
+    setResponseStatus(event, 401)
+    return {
+        error: 'An error occurred'
+    }
+})
---
+++ server/utils/user.js
@@ -0,0 +1,69 @@
+import { db } from '~/server/utils/mongo'
+import { compareSync } from 'bcrypt'
+
+function generateSessionId() {
+    const count = 64
+    let result = ''
+    const characters =
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    const charactersLength = characters.length
+    for (let i = 0; i < count; i++) {
+        result += characters.charAt(
+            Math.floor(Math.random() * charactersLength)
+        )
+    }
+    return result
+}
+
+export async function addSessionToUser(username, password) {
+    const user = await db.collection('users').findOne({ username: username })
+
+    if (!user) {
+        return {
+            error: 'Invalid username or password'
+        }
+    }
+
+    const match = compareSync(password, user.password)
+
+    if (!match) {
+        return {
+            error: 'Invalid username or password'
+        }
+    }
+
+    const sessionId = generateSessionId()
+
+    const expiresAt = new Date()
+    expiresAt.setFullYear(expiresAt.getFullYear() + 1)
+
+    await db.collection('users').updateOne(
+        { username: username },
+        {
+            $push: {
+                sessions: {
+                    sessionId: sessionId,
+                    expiresAt: expiresAt
+                }
+            }
+        }
+    )
+
+    return {
+        sessionId: sessionId,
+        expiresAt: expiresAt
+    }
+}
+
+export async function getUserBySessionId(sessionId) {
+    return await db.collection('users').findOne({
+        sessions: {
+            $elemMatch: {
+                sessionId: sessionId,
+                expiresAt: {
+                    $gt: new Date()
+                }
+            }
+        }
+    })
+}
