7f19126c3384c5859d97c683f6b6f2c3bc5f3d2c
vovastelmashchuk <vovochkastelmashchuk@gmail.com>
2024-11-25 23:30:18 +0200

    make image all secure

--- server/api/admin/image/all.get.js
+++ server/api/admin/image/all.get.js
@@ -1,12 +1,18 @@
-import { defineEventHandler } from 'h3'
+import { defineEventHandler, createError } from 'h3'
 import { db } from '~/server/utils/mongo'
 import * as dotenv from 'dotenv'
 
 dotenv.config()
 const imageDomain = process.env.IMAGE_DOMAIN
 
-
 export default defineEventHandler(async (event) => {
+    const isAuth = event.context?.auth?.username || false;
+    if (!isAuth) {
+        throw createError({
+            statusCode: 401,
+            statusMessage: 'Unauthorized',
+        });
+    }
     const images = await db.collection('images').find().toArray()
     return {
         images: images.map(image => ({
