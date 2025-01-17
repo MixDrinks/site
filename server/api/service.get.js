5b88f2b7d41adb894b0d6c2f1b5711005d886e75
Vova Stelmashchuk <vovochkastelmashchuk@gmail.com>
2025-01-18 00:10:48 +0200

    add commit sha to service

--- server/api/service.get.js
+++ server/api/service.get.js
@@ -1,5 +1,6 @@
 import { defineEventHandler } from 'h3'
 import { db } from '~/server/utils/mongo'
+import { getCommitSha } from '~~/server/utils/config'
 
 export default defineEventHandler(async (event) => {
     const response = {}
@@ -8,8 +9,18 @@ export default defineEventHandler(async (event) => {
     } else {
         response.isDbConnected = true
     }
+    let commitSha = 'init unknown'
+
+    try {
+        commitSha = getCommitSha()
+    } catch (e) {
+        console.error(e)
+        commitSha = `Error getting commit sha ${e}`
+    }
+
     const safeUsername = event.context?.auth?.username || 'User not logged in'
     response.username = safeUsername
     response.isAuth = !!event.context?.auth?.username
+    response.commitSha = commitSha
     return response
 })
